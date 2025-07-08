54293a5783b363e2e9613ff8b4f8fd20491ee27c
Celina Shrestha <celina.shrestha4@gmail.com>
2025-07-08 18:04:58 -0500

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -15,7 +15,7 @@ app.use(cors())
 mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => console.log(`Connected to MongoDB`))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err.message))
 
 app.use(routes)
 
